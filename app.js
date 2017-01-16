1df5d22a77ea9868a175f03b1df96b43be571076
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-16 14:46:09 -0600

    injected ngRoute, declared two routes and two controllers

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+console.log("welcome to route routing");
+
+//Inject ngRoute into your applcation
 var app = angular.module('RouteRounting', ['ngRoute'])
+
+
+// config section for your application where you set up two routes.
+app.config(function($routeProvider){
+    //Famous highway #1
+    $routeProvider.when('/', {
+        controller: 'MainCtrl',
+        templateUrl: 'partials/highwayone.html'
+    //Famous highway #2
+    }).when('/highwaytwo', {
+        controller: 'SecondCtrl',
+        templateUrl: 'partials/highwaytwo.html'
+    })
+})
+
+// Two controllers defined in your application
+app.controller('MainCtrl', function($scope){
+    console.log("MainCtrl");
+    $scope.highway = "Route 1"
+})
+
+app.controller('SecondCtrl', function($scope){
+    console.log("SecondCtrl");
+    $scope.highway = "Blue Ridge Parkway"
+
+})
